3033b08c20f2de21e1f416406ef79fb748207448
Ana Gabriele <gabbanasoares@gmail.com>
2021-10-07 13:38:58 -0700

    Learning about branches - this is only a test

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ def home():
 def about():
     return 'We are a non-profit organization working as an animal rescue. We aim to help you connect with the purrfect furbaby for you! The animals you find on our website are rescued and rehabilitated animals. Our mission is to promote the ideology "adopt, dont hop"! '
 
+@app.route('/testing_git_branches')
+def any_function():
+    return "Learning"
 
 
 if  __name__ == "__main__":
